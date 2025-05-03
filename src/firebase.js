27b7d48e59453181191aa2042b3429dda4d0e8d7
Kartik Jain <karthikjain@192.168.1.2>
2025-05-03 20:38:48 +0530

    env update

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8L2yEzdQADdlj7ZbiDT-JhiXuEaB7CCU",
-  authDomain: "vaultbox-cc15a.firebaseapp.com",
-  projectId: "vaultbox-cc15a",
-  storageBucket: "vaultbox-cc15a.appspot.com", // ✅ FIXED typo here!
-  messagingSenderId: "291532692792",
-  appId: "1:291532692792:web:508d77de92d281fa8b35cd",
-  measurementId: "G-KXXZYT7X0P"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
